fc7465d53c914ab2a4717149cd9b491e0295ef40
bhupathi2628 <bhupathi2628@gmail.com>
2021-08-17 18:48:53 +0530

    first commit

---
+++ Jenkinsinstallation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#The steps followed and the commands used to install Jenkins on Ubuntu 20 are as follows:
+
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+sudo systemctl status jenkins
